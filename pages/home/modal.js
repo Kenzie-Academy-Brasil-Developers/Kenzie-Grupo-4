ae3bb58a1d82dc115140e1a2c572d429417e51e2
Gabriel Gonzales <106750788+GabrielGonzalesKZ@users.noreply.github.com>
2022-11-09 13:39:35 -0300

    feat: estilos e responsividade

--- pages/home/modal.js
+++ pages/home/modal.js
@@ -15,3 +15,46 @@ const handleFocusOut = ({ target }) => {
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+
+
+const modalCard = (element) => {
+  const section = document.querySelector(".wallpaper")
+  const modal = document.createElement("div")
+  const modalPerfil = document.createElement("div")
+  const modalPerfilImg = document.createElement("div")
+  const modalImg = document.createElement("img")
+  const modalLevel = document.createElement("p")
+  const modalText = document.createElement("div")
+  const modalNick = document.createElement("p")
+  const modalRank = document.createElement("div")
+  const modalPoints = document.createElement("p")
+  const modalRankLogo = document.createElement("div")
+  const modalCard = document.createElement("div")
+  const modalDescriptions = document.createElement("div")
+  const modalChampions = document.createElement("div")
+  const modalChampionsTitle = document.createElement("p")
+  const modalUl = document.createElement("ul")
+  const modalLi = document.createElement("li")
+  const modalChampionsImg = document.createElement("img")
+  const modalMaestry = document.createElement("p")
+  const modalWinrate = document.createElement("div")
+  const modalWinrateTitle = document.createElement("p")
+  const modalWinrateP = document.createElement("p")
+
+  modal.classList.add(".modal")
+  modalPerfil.classList.add("modal__perfil")
+  modalPerfilImg.classList.add("modal__perfil__img")
+  modalImg.classList.add("modal__img")
+  modalLevel.classList.add("modal__level")
+  modalText.classList.add("modal__text")
+  modalNick.classList.add("modal__nick")
+  modalRank.classList.add("modal__rank")
+  modalPoints.classList.add("modal__rank__poinst")
+
+
+
+
+
+
+
+}
